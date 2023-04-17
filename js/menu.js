f1ba27d6f22208be260c8dd05ed325c58e394abb
Hugo Mesquita Nunes <hugomn03@hotmail.com>
2023-04-17 15:22:45 -0300

    pagina feita

---
+++ js/menu.js
@@ -0,0 +1,25 @@
+function menuShow() {
+  let menuMobile = document.querySelector(".mobile-menu");
+  if (menuMobile.classList.contains("open")) {
+    menuMobile.classList.remove("open");
+    document.querySelector(".icon").src = "../img/menu-responsivo.png";
+  } else {
+    menuMobile.classList.add("open");
+    document.querySelector(".icon").src = "../img/menu-responsivo.png";
+  }
+}
+const cookieBox = document.querySelector(".wrapper");
+acceptBtn = cookieBox.querySelector(".bttn button");
+
+acceptBtn.onclick = () => {
+  document.cookie = "CookieBy=CodingNepal; max-age=" + 60 * 60 * 24 * 30;
+  if (document.cookie) {
+    cookieBox.classList.add("hide");
+  } else {
+    alert("Cookie can't be set!");
+  }
+};
+let checkCookie = document.cookie.indexOf("CookieBy=CodingNepal");
+checkCookie != 1
+  ? cookieBox.classList.add("hide")
+  : cookieBox.classList.remove("hide");
